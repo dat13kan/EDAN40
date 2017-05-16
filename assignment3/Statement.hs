7bba68cfcaec160b2c9179b13849bdda0adac025
Kevin <KOOL_music@hotmail.com>
2017-05-16 13:14:17 +0200

    Parse in statement done

--- assignment3/Statement.hs
+++ assignment3/Statement.hs
@@ -8,9 +8,9 @@ data Statement =
     Assignment String Expr.T |
     If Expr.T Statement Statement |
     Skip |
-    Begin Statement |
+    Begin [Statement] |
     While Expr.T Statement |
-	Read String |
+    Read String |
     Write Expr.T
 
     deriving Show
@@ -27,14 +27,18 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
 skipStatement = accept "skip" # require ";" >-> buildSkip
 buildSkip _ = Skip
 
+ifStatement = accept "if" -# Expr.parse # require "then" -# parse # require "else" -# parse >-> buildIf
+buildIf ((e, x), y) = If e x y
+
 beginStatement = accept "begin" -# iter parse #- require "end" >-> Begin
 
-whileStatement = accept "while" -# Expr.parse #- require "do" >-> While
+whileStatement = accept "while" -# Expr.parse # require "do" -# parse >-> buildWhile
+buildWhile (e, x) = While e x
 
 readStatement = accept "read" -# word #- require ";" >-> Read
 
 writeStatement = accept "write" -# Expr.parse #- require ";" >-> Write
 
 instance Parse Statement where
-  parse = assignment ! exec ! skipStatement ! beginStatement ! whileStatement ! readStatement ! writeStatement
+  parse = assignment ! ifStatement ! skipStatement ! beginStatement ! whileStatement ! readStatement ! writeStatement
   toString = error "Statement.toString not implemented"
